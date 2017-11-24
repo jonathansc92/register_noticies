4ff1eea99174a621dbe2c3d1cd5951b6cbab1a43
Jonathan Cruz <jonathansc92@gmail.com>
2017-11-24 11:36:21 -0200

    Module - Teste with modules

--- app.js
+++ app.js
@@ -2,6 +2,9 @@
 
 // Importar modulo express para criar o servidor
 var express = require('express');
+var msg = require('./mod_test')();
+
+
 var app = express();
 
 app.set('view engine', 'ejs');
@@ -30,6 +33,6 @@ app.get('/noticias', function(req, res){
 });
 
 app.listen(3000, function(){
-	console.log('Servidor rodando com express');
+	console.log(msg);
 
 });
