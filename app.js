e430e2da52d212ae41b19acdec15d048a82f47dd
Jonathan Cruz <jonathansc92@gmail.com>
2017-11-24 12:37:24 -0200

    APP - Center the routes

--- app.js
+++ app.js
@@ -1,24 +1,8 @@
 var app = require('./config/server');
 
-app.get('/', function(req, res){
-
-	res.render('home/index');
-
-});
-
-
-app.get('/formulario_inclusao_noticia', function(req, res){
-
-	res.render('admin/form_add_noticia');
-
-});
-
-
-app.get('/noticias', function(req, res){
-
-	res.render('noticias/noticias');
-
-});
+var rotaNoticias = require('./app/routes/noticias')(app);
+var rotaHome = require('./app/routes/home')(app);
+var rotaFormulario = require('./app/routes/formulario_inclusao_noticia')(app);
 
 app.listen(3000, function(){
 	console.log("ON");
--- config/server.js
+++ config/server.js
@@ -1,8 +1,7 @@
 // Importar modulo express para criar o servidor
 var express = require('express');
-
 var app = express();
-
 app.set('view engine', 'ejs');
+app.set('views', './app/views');
 
 module.exports = app;
