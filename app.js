924d248ab2bfd33f6e98157b9c34f3ab58bc4562
Jonathan Cruz <jonathansc92@gmail.com>
2017-11-24 11:15:20 -0200

    Views - add new files views

--- app.js
+++ app.js
@@ -7,18 +7,27 @@ var app = express();
 app.set('view engine', 'ejs');
 
 
-app.get('/tecnologia', function(req, res){
 
-	res.render('secao/tecnologia');
-
-});
 
 app.get('/', function(req, res){
 
-	res.send('<html><body>tecnologia</body></html>');
+	res.render('home/index');
 
 });
 
+
+app.get('/formulario_inclusao_noticia', function(req, res){
+
+	res.render('admin/form_add_noticia');
+
+});
+
+
+app.get('/noticias', function(req, res){
+
+	res.render('noticias/noticias');
+
+});
 
 app.listen(3000, function(){
 	console.log('Servidor rodando com express');
