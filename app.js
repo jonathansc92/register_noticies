954b89eb9331cc3d7816432e34c06915cc552c88
Jonathan Cruz <jonathansc92@gmail.com>
2017-11-24 11:09:04 -0200

    Organization using EJS in app

--- app.js
+++ app.js
@@ -4,14 +4,16 @@
 var express = require('express');
 var app = express();
 
+app.set('view engine', 'ejs');
 
-app.get('/', function(req, res){
 
-	res.send('<html><body>Portal</body></html>');
+app.get('/tecnologia', function(req, res){
+
+	res.render('secao/tecnologia');
 
 });
 
-app.get('/tecnologia', function(req, res){
+app.get('/', function(req, res){
 
 	res.send('<html><body>tecnologia</body></html>');
 
