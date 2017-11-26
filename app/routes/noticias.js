e1f333091c0f213e5ae6b59614f7e4a08146e8e6
Jonathan Cruz <jonathansc92@gmail.com>
2017-11-26 18:12:55 -0200

    DB - Refactoring configuration Database

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -1,26 +1,15 @@
+var db = require('../../config/db');
+
 
 module.exports = function(app){
 
 	app.get('/noticias', function(req, res){
 
-	var mysql = require('mysql');
-
-	var connection = mysql.createConnection({
-		host: 'localhost',
-		user: 'root',
-		password: '',
-		database: 'portal_noticias'
-	});
-
-
-	connection.query('select * from noticias', function(error, result){
-		res.render('noticias/noticias', {noticias: result});
-	});
-
-
-	
-
-		//
+		var connection = db();	
+		
+		connection.query('select * from noticias', function(error, result){
+			res.render('noticias/noticias', {noticias: result});
+		});
 
 	});
 };
--- config/server.js
+++ config/server.js
@@ -1,7 +1,7 @@
-// Importar modulo express para criar o servidor
-var express = require('express');
-var app = express();
-app.set('view engine', 'ejs');
-app.set('views', './app/views');
+	// Importar modulo express para criar o servidor
+	var express = require('express');
+	var app = express();
+	app.set('view engine', 'ejs');
+	app.set('views', './app/views');
 
-module.exports = app;
+	module.exports = app;
