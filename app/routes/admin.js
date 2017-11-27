4584dbbf1535a3643b5bf9298c0d22bf461087e3
Jonathan Cruz <jonathansc92@gmail.com>
2017-11-27 10:59:43 -0200

    Noticia - Show validation errors in register

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -9,7 +9,7 @@ module.exports = function(app){
 	app.post('/noticias/salvar', function(req, res){
 		var noticia = req.body;
 		//validacao, conexao, model(DAO), salva
-		console.log(noticia);
+		//console.log(noticia);
 		req.assert('titulo','Título é obrigatório').notEmpty();
 		req.assert('resumo','Resumo é obrigatório').notEmpty();
 		req.assert('resumo','Resumo deve conter entre 10 e 100 caracteres').len(10, 100);
@@ -17,11 +17,12 @@ module.exports = function(app){
 		//req.assert('data_noticia','Data é obrigatório').notEmpty().isDate({format: 'YYYY-MM-DD'});
 		req.assert('noticia','Noticia é obrigatório').notEmpty();
 
-
 		var errors = req.validationErrors();
 
+		console.log(errors);
+
 		if(errors){
-			res.render('admin/form_add_noticia');
+			res.render('admin/form_add_noticia', {validacao: errors});
 			return;
 		}
 
